af1f5380fefd01cbf6e20ea366b010ac4bcefe5d
NestZ <NestZ671@gmail.com>
2021-02-06 16:24:27 +0700

    init media module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PingModule } from './ping/ping.module';
+import { MediaModule } from './media/media.module';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { PingModule } from './ping/ping.module';
       autoSchemaFile: 'schema.gql',
     }),
     PingModule,
+    MediaModule,
   ],
   controllers: [AppController],
   providers: [AppService],
