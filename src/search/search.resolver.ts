feb5f2b807497303dbbd987730c8886178ab8bdb
NestZ <NestZ671@gmail.com>
2021-03-17 21:04:54 +0700

    add search route

--- src/search/search.resolver.ts
+++ src/search/search.resolver.ts
@@ -1,7 +1,7 @@
 import { Resolver, Mutation, Query, Args } from '@nestjs/graphql';
 import { SearchService } from './search.service';
-import { StatusCode } from './search.model';
-import { SearchArgs } from './search.dto';
+import { StatusCode, SearchBody } from './search.model';
+import { SearchArgs, DeleteArgs } from './search.dto';
 
 @Resolver()
 export class SearchResolver {
@@ -12,6 +12,27 @@ export class SearchResolver {
     @Args({ name: 'args', type: () => SearchArgs }) args: SearchArgs,
   ): Promise<string[]> {
     return await this.searchService.search(args);
+  }
+
+  @Mutation(() => StatusCode)
+  async indexMedia(
+    @Args({ name: 'args', type: () => SearchBody }) args: SearchBody,
+  ): Promise<StatusCode> {
+    return await this.searchService.indexMedia(args);
+  }
+
+  @Mutation(() => StatusCode)
+  async updateIndex(
+    @Args({ name: 'args', type: () => SearchBody }) args: SearchBody,
+  ): Promise<StatusCode> {
+    return await this.searchService.update(args);
+  }
+
+  @Mutation(() => StatusCode)
+  async removeIndex(
+    @Args({ name: 'args', type: () => DeleteArgs }) args: DeleteArgs,
+  ): Promise<StatusCode> {
+    return await this.searchService.remove(args);
   }
 
   @Mutation(() => StatusCode)
